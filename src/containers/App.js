d28a70060bc9d325856663f485c1de6dd0037593
James Rodney <jlrodney95@gmail.com>
2019-01-10 15:13:40 +0000

    setting up Redux config

---
+++ src/containers/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  render() {
+      return (
+        <div>
+          <header>
+          <h1 className="header">Budgeting App</h1>
+          </header>
+      </div>
+      );
+  }
+}
+
+export default App;
