3498cf7beb8ba7b6d555d542beda21ecc4b4f413
James Rodney <jlrodney95@gmail.com>
2019-01-15 16:49:17 +0000

    setup server and build

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path')
+const app = express();
+
+const buildPath = path.join(__dirname, '..', 'build')
+
+const port = provess.env.PORT || 3000;
+
+app.use(express.static(buildPath));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(buildPath, 'index.html'));
+})
+
+app.listen(port, () => {
+  console.log('Server running');
+})
