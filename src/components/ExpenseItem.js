a4a0526db9600fdb470735264fca44fb1903fb2b
James Rodney <jlrodney95@gmail.com>
2019-01-15 17:33:38 +0000

    Modification to date and amount format

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
 
 const ExpenseItem = ({ id, description, amount, createdAt }) => (
   <div>
     <h2>{description}</h2>
-    <span className="data">Amount: £{amount/100} - Timestamp: {createdAt}</span>
+    <span className="data">
+    Amount: £{(amount/100).toFixed(2)} - Date: {moment(createdAt).format('Do MMMM YYYY')}
+    </span>
     <span className="edit-link"><Link to={`/edit/${id}`}>Edit</Link></span>
     <span className="remove-link"><Link to={`/remove/${id}`}>Remove</Link></span>
   </div>
