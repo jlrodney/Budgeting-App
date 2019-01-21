5686b188d35cfc34cec299a53112d9676dc34c3f
James Rodney <jlrodney95@gmail.com>
2019-01-21 19:16:45 +0000

    Modified LoginPage

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -4,6 +4,9 @@ import startLogin from '../actions/auth';
 
 export const LoginPage = ({ startLogin }) => (
   <div>
+  <h1 className="header">Budgeting App</h1>
+  <h2>This site will allow you to log your expenses and will help you manage your budget</h2>
+  <h2>To access your spending please login</h2>
   <button onClick={startLogin}>Login</button>
   </div>
 );
